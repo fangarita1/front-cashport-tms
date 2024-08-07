1fcc2132748b08f5274eed9585af6173380b5395
DaniReus04 <daniel_carvalho_amaroo@outlook.com>
2024-08-07 12:30:31 -0300

    fix: layout children import

--- src/app/logistics/transfer-request/layout.tsx
+++ src/app/logistics/transfer-request/layout.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from "next";
-import CreateTransferRequestPage from "./page";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Solicitud de transferencia",
   description: "Solicitud de transferencia"
 };
 
-export default CreateTransferRequestPage;
+export default function TransferRequestLayout({children}: {children: React.ReactNode}) {
+  return <>{children}</>
+}
