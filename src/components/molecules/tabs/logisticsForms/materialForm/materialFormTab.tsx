28d73562005a44868149ede6123eaf6983f7d0c3
vickycabrera <vlcabrera92@hotmail.com>
2024-08-21 17:50:12 -0300

    resolve building

--- src/components/molecules/tabs/logisticsForms/materialForm/materialFormTab.tsx
+++ src/components/molecules/tabs/logisticsForms/materialForm/materialFormTab.tsx
@@ -1,12 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  Button,
-  Col,
-  Flex,
-  Form,
-  Row,
-  Typography
-} from "antd";
+import { Button, Col, Flex, Form, Row, Typography } from "antd";
 import { Controller, useForm } from "react-hook-form";
 import { ArrowsClockwise, CaretLeft, Pencil } from "phosphor-react";
 
@@ -21,12 +14,15 @@ import {
   _onSubmit,
   dataToProjectFormData,
   validationButtonText,
-  MaterialFormTabProps,
+  MaterialFormTabProps
 } from "./materialFormTab.mapper";
-import {  IFormMaterial, IMaterialType, IMaterialTransportType } from "@/types/logistics/schema";
+import { IFormMaterial, IMaterialType, IMaterialTransportType } from "@/types/logistics/schema";
 import { InputDateForm } from "@/components/atoms/inputs/InputDate/InputDateForm";
 
-import { UploadDocumentButton } from "@/components/atoms/UploadDocumentButton/UploadDocumentButton";
+import {
+  FileObject,
+  UploadDocumentButton
+} from "@/components/atoms/UploadDocumentButton/UploadDocumentButton";
 
 import useSWR from "swr";
 import { getDocumentsByEntityType } from "@/services/logistics/certificates";
@@ -39,7 +35,12 @@ import UploadDocumentChild from "@/components/atoms/UploadDocumentChild/UploadDo
 import SubmitFormButton from "@/components/atoms/SubmitFormButton/SubmitFormButton";
 import LoadDocumentsButton from "@/components/atoms/LoadDocumentsButton/LoadDocumentsButton";
 import { SelectInputForm } from "@/components/molecules/logistics/SelectInputForm/SelectInputForm";
-import { bloodTypesOptions, documentTypesOptions, glassesOptions, licencesOptions } from "../formSelectOptions";
+import {
+  bloodTypesOptions,
+  documentTypesOptions,
+  glassesOptions,
+  licencesOptions
+} from "../formSelectOptions";
 import MultiSelectTags from "@/components/ui/multi-select-tags/MultiSelectTags";
 
 const { Title, Text } = Typography;
@@ -52,7 +53,7 @@ export const MaterialFormTab = ({
   onActiveProject = () => {},
   onDesactivateProject = () => {},
   params,
-  handleFormState = () => {},
+  handleFormState = () => {}
 }: MaterialFormTabProps) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [isOpenModalDocuments, setIsOpenModalDocuments] = useState(false);
@@ -80,7 +81,7 @@ export const MaterialFormTab = ({
   interface ImageState {
     file: File | undefined;
   }
-  
+
   const [images, setImages] = useState<ImageState[]>(
     Array(5).fill({ file: undefined, error: false })
   );
@@ -93,40 +94,36 @@ export const MaterialFormTab = ({
     control,
     handleSubmit,
     reset,
-    formState: { errors, isValid}
+    formState: { errors, isValid }
   } = useForm<IFormMaterial>({
     defaultValues,
     disabled: statusForm === "review"
   });
 
-  const formImages = watch("images")
+  const formImages = watch("images");
 
   const hasImages = () => {
-    return (images.some(img=>img.file) || (formImages && formImages.length > 0))
-  }
+    return images.some((img) => img.file) || (formImages && formImages.length > 0);
+  };
   const isFormCompleted = () => {
-    return isValid && hasImages()
-  }
+    return isValid && hasImages();
+  };
 
-  const isSubmitButtonEnabled = isFormCompleted() && !loading
+  const isSubmitButtonEnabled = isFormCompleted() && !loading;
   /*archivos*/
-  interface FileObject {
-    docReference: string;
-    file: File | undefined;
-  }
+
   const [files, setFiles] = useState<(FileObject & { aditionalData?: any })[]>([]);
 
   useEffect(() => {
     if (Array.isArray(documentsType)) {
-      const isFirstLoad = data?.documents?.length && selectedFiles.length === 0 
+      const isFirstLoad = data?.documents?.length && selectedFiles.length === 0;
       if (isFirstLoad) {
-
         const docsWithLink =
           documentsType
             ?.filter((f) => data.documents?.find((d) => d.id_document_type === f.id))
             .map((f) => ({
               ...f,
-              file:  undefined,
+              file: undefined,
               link: data.documents?.find((d) => d.id_document_type === f.id)?.url_archive,
               expirationDate: dayjs(
                 data.documents?.find((d) => d.id_document_type === f.id)?.expiration_date
@@ -134,17 +131,18 @@ export const MaterialFormTab = ({
             })) || [];
         setSelectedFiles(docsWithLink);
       } else {
-        
-        const documentsFiltered = documentsType?.filter((f) => !f?.optional || selectedFiles?.find((f2) => f2.id === f.id))
-        const docsWithFile =  documentsFiltered.map((f) => {
-            const prevFile = selectedFiles.find((f2) => f2.id === f.id);
-            return {
-              ...f,
-              link: prevFile?.link || undefined,
-              file: prevFile?.link ? undefined : files.find((f2) => f2.aditionalData === f.id)?.file,
-              expirationDate: prevFile?.expirationDate
-            };
-          });
+        const documentsFiltered = documentsType?.filter(
+          (f) => !f?.optional || selectedFiles?.find((f2) => f2.id === f.id)
+        );
+        const docsWithFile = documentsFiltered.map((f) => {
+          const prevFile = selectedFiles.find((f2) => f2.id === f.id);
+          return {
+            ...f,
+            link: prevFile?.link || undefined,
+            file: prevFile?.link ? undefined : files.find((f2) => f2.aditionalData === f.id)?.file,
+            expirationDate: prevFile?.expirationDate
+          };
+        });
         if (docsWithFile?.length) {
           setSelectedFiles([...docsWithFile]);
         } else {
@@ -155,20 +153,20 @@ export const MaterialFormTab = ({
   }, [files, documentsType]);
 
   useEffect(() => {
-    if (statusForm === "review"){
+    if (statusForm === "review") {
       if (Array.isArray(documentsType)) {
-          const docsWithLink =
-            documentsType
-              ?.filter((f) => data?.documents?.find((d) => d.id_document_type === f.id))
-              .map((f) => ({
-                ...f,
-                file:  undefined,
-                link: data?.documents?.find((d) => d.id_document_type === f.id)?.url_archive,
-                expirationDate: dayjs(
-                  data?.documents?.find((d) => d.id_document_type === f.id)?.expiration_date
-                )
-              })) || [];
-          setSelectedFiles(docsWithLink);
+        const docsWithLink =
+          documentsType
+            ?.filter((f) => data?.documents?.find((d) => d.id_document_type === f.id))
+            .map((f) => ({
+              ...f,
+              file: undefined,
+              link: data?.documents?.find((d) => d.id_document_type === f.id)?.url_archive,
+              expirationDate: dayjs(
+                data?.documents?.find((d) => d.id_document_type === f.id)?.expiration_date
+              )
+            })) || [];
+        setSelectedFiles(docsWithLink);
       }
     }
   }, [statusForm]);
@@ -213,7 +211,6 @@ export const MaterialFormTab = ({
           };
         });
       });
-      
     }
   };
 
@@ -222,14 +219,14 @@ export const MaterialFormTab = ({
       (item) => item.id === data.general.license_category
     )?.value;
     data.general.rhval = bloodTypesOptions.find((item) => item.id === data.general.rh)?.value;
-    data.general.vehicle_type = data.general.vehicle_type.map((v:any)=>v.value)
+    data.general.vehicle_type = data.general.vehicle_type.map((v: any) => v.value);
     _onSubmit(
       data,
       selectedFiles,
       imageFile ? [{ docReference: "imagen", file: imageFile }] : undefined,
       setImageError,
       setLoading,
-      onSubmitForm,
+      onSubmitForm
     );
   };
 
@@ -252,7 +249,7 @@ export const MaterialFormTab = ({
               <Button
                 className="buttons"
                 htmlType="button"
-                disabled={statusForm === "review"}  
+                disabled={statusForm === "review"}
                 onClick={(e) => {
                   e.preventDefault();
                   setIsOpenModal(true);
@@ -262,41 +259,43 @@ export const MaterialFormTab = ({
                 <ArrowsClockwise size={"1.2rem"} />
               </Button>
             )}
-              {statusForm === "review" ? (
-                <Button
-                  className="buttons -edit"
-                  htmlType="button"
-                  onClick={(e) => {
-                    handleFormState("edit")
-                    e.preventDefault();
-                  }}
-                >
-                  {validationButtonText(statusForm)}
-                  <Pencil size={"1.2rem"} />
-                </Button>
-              ) : (
-                ""
-              )}
-              {statusForm === "edit" ? (
-                <Button
-                  className="buttons -edit"
-                  htmlType="button"
-                  onClick={(e) => {
-                    handleFormState("review")
-                    e.preventDefault();
-                    reset()
-                  }}
-                >
-                  {"Cancelar edición"}
-                </Button>
-              ) : (
-                ""
-              )}
+            {statusForm === "review" ? (
+              <Button
+                className="buttons -edit"
+                htmlType="button"
+                onClick={(e) => {
+                  handleFormState("edit");
+                  e.preventDefault();
+                }}
+              >
+                {validationButtonText(statusForm)}
+                <Pencil size={"1.2rem"} />
+              </Button>
+            ) : (
+              ""
+            )}
+            {statusForm === "edit" ? (
+              <Button
+                className="buttons -edit"
+                htmlType="button"
+                onClick={(e) => {
+                  handleFormState("review");
+                  e.preventDefault();
+                  reset();
+                }}
+              >
+                {"Cancelar edición"}
+              </Button>
+            ) : (
+              ""
+            )}
           </Flex>
         </Flex>
-        <Flex component={"main"} flex="1" vertical style={{paddingRight: "1rem"}}>
-          <Row gutter={[16,16]}> 
-            <Col span={6}>  {/* Columna Fotos del Vehiculo */}
+        <Flex component={"main"} flex="1" vertical style={{ paddingRight: "1rem" }}>
+          <Row gutter={[16, 16]}>
+            <Col span={6}>
+              {" "}
+              {/* Columna Fotos del Vehiculo */}
               <Title className="title" level={4}>
                 Fotos del material
               </Title>
@@ -326,20 +325,22 @@ export const MaterialFormTab = ({
                     <Text className="textError">{"Al menos 1 imagen debe ser cargada *"}</Text>
                   )}
                 </Col>
-                </Row>
-                <Row gutter={16}>
+              </Row>
+              <Row gutter={16}>
                 {images.slice(1).map((image, index) => (
                   <Col xs={24} sm={12} lg={6} className="colfotomin" key={index + 1}>
                     <UploadImg
                       disabled={statusForm === "review"}
-                      imgDefault={formImages ? formImages[index+1]?.url_archive : undefined}
+                      imgDefault={formImages ? formImages[index + 1]?.url_archive : undefined}
                       setImgFile={(file) => {
-                      const currentUrlArchive = formImages ? formImages[index+1]?.url_archive : undefined; // obtener el valor actual de url_archive
+                        const currentUrlArchive = formImages
+                          ? formImages[index + 1]?.url_archive
+                          : undefined; // obtener el valor actual de url_archive
                         if (currentUrlArchive) {
                           (file as any).url_archive = currentUrlArchive;
-                          setValue(`images.${index+1}`, file);
+                          setValue(`images.${index + 1}`, file);
                         } else {
-                          setValue(`images.${index+1}`, file);
+                          setValue(`images.${index + 1}`, file);
                         }
                         setImages((prev) =>
                           prev.map((img, imgIndex) =>
@@ -355,16 +356,19 @@ export const MaterialFormTab = ({
                 ))}
               </Row>
             </Col>
-            <Col span={18}> {/* Columna Informacion general*/}
+            <Col span={18}>
+              {" "}
+              {/* Columna Informacion general*/}
               <Row>
                 <Col span={24}>
                   <Title className="title" level={4}>
-                      Datos del material
+                    Datos del material
                   </Title>
                 </Col>
               </Row>
-
-              <Row gutter={[16,16]}> {/* Fila campos info gral*/}
+              <Row gutter={[16, 16]}>
+                {" "}
+                {/* Fila campos info gral*/}
                 <Col span={12}>
                   <InputForm
                     titleInput="Código"
@@ -412,66 +416,69 @@ export const MaterialFormTab = ({
                     control={control}
                     error={errors?.general?.kg_weight}
                   />
-                </Col>                                        
-                
+                </Col>
               </Row>
             </Col>
           </Row>
           {/* ----------------------------------Vehiculos--------------------------------- */}
 
           <Row style={{ width: "100%", marginTop: "2rem" }}>
-              <Col span={24}>
-                <Title className="title" level={4}>
-                  Características
-                </Title>
-                <Row style={{ width: "100%", marginTop: "2rem" }}>
-                  <Col span={24}>
-                    <Controller
-                      name="general.material_transport"
-                      control={control}
-                      rules={{ required: true }}
-                      render={({ field }) => 
-                        <MultiSelectTags
-                          field={field}
-                          placeholder="Seleccionar"
-                          title="Características de transporte"
-                          errors={errors?.general?.material_type}
-                          options={convertToSelectOptionsTransport((materialsTransportTypesData?.data.data as any) || [])}
-                          disabled={statusForm === "review"} 
-                        />
-                      }
-                    />
-                  </Col>
-                  <Col span={24}>
-                    <Controller
-                      name="general.material_type"
-                      control={control}
-                      rules={{ required: true }}
-                      render={({ field }) => 
-                        <MultiSelectTags
-                          field={field}
-                          placeholder="Seleccionar"
-                          title="Características de seguridad"
-                          errors={errors?.general?.material_type}
-                          options={convertToSelectOptions((materialsTypesData?.data.data as any) || [])}
-                          disabled={statusForm === "review"} 
-                        />
-                      }
-                    />
-                  </Col>                  
-                </Row>
-              </Col>              
+            <Col span={24}>
+              <Title className="title" level={4}>
+                Características
+              </Title>
+              <Row style={{ width: "100%", marginTop: "2rem" }}>
+                <Col span={24}>
+                  <Controller
+                    name="general.material_transport"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <MultiSelectTags
+                        field={field}
+                        placeholder="Seleccionar"
+                        title="Características de transporte"
+                        errors={errors?.general?.material_type}
+                        options={convertToSelectOptionsTransport(
+                          (materialsTransportTypesData?.data.data as any) || []
+                        )}
+                        disabled={statusForm === "review"}
+                      />
+                    )}
+                  />
+                </Col>
+                <Col span={24}>
+                  <Controller
+                    name="general.material_type"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <MultiSelectTags
+                        field={field}
+                        placeholder="Seleccionar"
+                        title="Características de seguridad"
+                        errors={errors?.general?.material_type}
+                        options={convertToSelectOptions(
+                          (materialsTypesData?.data.data as any) || []
+                        )}
+                        disabled={statusForm === "review"}
+                      />
+                    )}
+                  />
+                </Col>
+              </Row>
+            </Col>
           </Row>
 
           {["edit", "create"].includes(statusForm) && (
-            <Row justify={"end"} >
+            <Row justify={"end"}>
               <SubmitFormButton
-                  text={validationButtonText(statusForm)}
-                  disabled={!isSubmitButtonEnabled}
-                  onClick={handleSubmit(onSubmit)}
-                />
+                text={validationButtonText(statusForm)}
+                disabled={!isSubmitButtonEnabled}
+                onClick={handleSubmit(onSubmit)}
+              />
             </Row>
-          )} 
+          )}
         </Flex>
       </Form>
       <ModalChangeStatus
