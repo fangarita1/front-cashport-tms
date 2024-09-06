5a4abcaaa0ff243fd68c89cb5804cfc46d48514d
Marcos <84048657+Rodriguez-Marcos@users.noreply.github.com>
2024-09-06 11:44:15 -0300

    novelty

--- src/components/organisms/logistics/transfer-orders/details/drawer-create-body/DrawerCreateBody.tsx
+++ src/components/organisms/logistics/transfer-orders/details/drawer-create-body/DrawerCreateBody.tsx
@@ -1,14 +1,25 @@
-import { Button, Input, Select, Typography, Upload } from 'antd';
-import styles from './drawerCreateBody.module.scss';
-import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
-import { CaretDoubleRight, ChartBar, ChartLineUp, Check, FileArrowDown, Files, Money, PlusCircle, User } from 'phosphor-react';
-import { getNoveltyTypes } from '@/services/logistics/novelty';
-import { INovelty, INoveltyType } from '@/types/novelty/INovelty';
-import { noveltyQuantity } from '@/utils/constants/novelties';
-import { IForm } from '../Details';
-import { UploadFile } from 'antd/lib';
-import { UploadChangeParam } from 'antd/es/upload';
-import { FileDownloadModal } from '@/components/molecules/modals/FileDownloadModal/FileDownloadModal';
+import { Button, Input, Select, Typography, Upload } from "antd";
+import { NumericFormat } from "react-number-format";
+import styles from "./drawerCreateBody.module.scss";
+import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
+import {
+  CaretDoubleRight,
+  ChartBar,
+  ChartLineUp,
+  Check,
+  FileArrowDown,
+  Files,
+  Money,
+  PlusCircle,
+  User
+} from "phosphor-react";
+import { getNoveltyTypes, getOvercosts } from "@/services/logistics/novelty";
+import { INovelty, INoveltyType } from "@/types/novelty/INovelty";
+import { noveltyQuantity } from "@/utils/constants/novelties";
+import { IForm } from "../Details";
+import { UploadFile } from "antd/lib";
+import { UploadChangeParam } from "antd/es/upload";
+import useSWR from "swr";
 
 const Text = Typography;
 
@@ -19,7 +30,8 @@ interface IDrawerBodyProps {
   form: IForm;
   setForm: Dispatch<SetStateAction<IForm>>;
   formEvidences: File[];
-  setFormEvidences: Dispatch<SetStateAction<File[]>>
+  setFormEvidences: Dispatch<SetStateAction<File[]>>;
+  tripData: { idCarrier: number; idVehicleType: number };
 }
 
 export const DrawerCreateBody: FC<IDrawerBodyProps> = ({
@@ -29,21 +41,34 @@ export const DrawerCreateBody: FC<IDrawerBodyProps> = ({
   setForm,
   novelty,
   formEvidences,
-  setFormEvidences
+  setFormEvidences,
+  tripData
 }) => {
   const [isDisabled, setIsDisabled] = useState<boolean>(true);
   const [novelties, setNovelties] = useState<INoveltyType[]>([]);
+  const { data: overcosts, isLoading: isLoadingOvercosts } = useSWR(
+    {
+      key: "overcosts",
+      idCarrier: tripData.idCarrier,
+      idVehicleType: tripData.idVehicleType
+    },
+    ({ idCarrier, idVehicleType }) => getOvercosts(idVehicleType, idCarrier),
+    {
+      keepPreviousData: false,
+      refreshInterval: 1000 * 60 * 60 * 24
+    }
+  );
 
   const findNoveltyTypes = async () => {
     const data = await getNoveltyTypes();
     if (data.length > 0) {
       setNovelties(data);
     }
-  }
+  };
 
   useEffect(() => {
     findNoveltyTypes();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (novelty && novelty.id) {
@@ -52,10 +77,10 @@ export const DrawerCreateBody: FC<IDrawerBodyProps> = ({
         noeltyTypeId: getNovelty?.id || null,
         quantity: novelty.quantity,
         observation: novelty.observation,
-        value: novelty.value,
+        value: novelty.value
       });
     }
-  }, [novelties])
+  }, [novelties]);
 
   useEffect(() => {
     if (form.noeltyTypeId && form.observation && form.value) {
@@ -63,13 +88,27 @@ export const DrawerCreateBody: FC<IDrawerBodyProps> = ({
       return;
     }
     setIsDisabled(true);
-  }, [form])
+  }, [form]);
+
+  useEffect(() => {
+    setForm((prev) => ({
+      ...prev,
+      value: calculateOvercost(form.quantity, form.overcostId)
+    }));
+  }, [form.quantity, form.overcostId, overcosts]);
+
+  const calculateOvercost = (quantity: number, overcostId?: number) => {
+    if (!overcostId) return 0;
+    const overcost = overcosts?.find((oc) => oc.id === overcostId);
+    if (overcost) {
+      return overcost.price * quantity;
+    }
+    return 0;
+  };
 
   const updateFiles = (newFile: File) => {
     setFormEvidences((prevState: File[]) => {
-      const existingFileIndex = prevState.findIndex(
-        (file) => file.name === newFile.name
-      );
+      const existingFileIndex = prevState.findIndex((file) => file.name === newFile.name);
 
       if (existingFileIndex !== -1) {
         const updatedFiles = [...prevState];
@@ -97,7 +136,7 @@ export const DrawerCreateBody: FC<IDrawerBodyProps> = ({
         console.warn("El archivo no contiene un File válido:", rawFile);
       }
     }
-  }
+  };
 
   return (
     <div className={styles.mainDrawerBody}>
@@ -111,12 +150,14 @@ export const DrawerCreateBody: FC<IDrawerBodyProps> = ({
         <div className={styles.btnContainer}>
           <Button
             onClick={handleCreateNovelty}
-            type='text'
+            type="text"
             className={`${styles.approbeBtn} ${isDisabled && styles.disabled}`}
             disabled={isDisabled}
           >
-            <Check color='#141414' size={12} />
-            <Text className={styles.approbeLabel}>{novelty && novelty.id ? 'Actualizar novedad' : 'Crear novedad'}</Text>
+            <Check color="#141414" size={12} />
+            <Text className={styles.approbeLabel}>
+              {novelty && novelty.id ? "Actualizar novedad" : "Crear novedad"}
+            </Text>
           </Button>
         </div>
       </div>
@@ -125,84 +166,116 @@ export const DrawerCreateBody: FC<IDrawerBodyProps> = ({
         <div className={styles.leftSection}>
           <div>
             <div className={styles.formTitleContainer}>
-              <ChartBar color='#666666' size={20} />
+              <ChartBar color="#666666" size={20} />
               <Text className={styles.formTitle}>Tipo de novedad</Text>
             </div>
             <Select
               className={styles.select}
-              value={form.noeltyTypeId}
+              value={form.noeltyTypeId || undefined}
               onChange={(val) => setForm({ ...form, noeltyTypeId: val })}
-              placeholder='Seleccionar origen o destino'
-              options={novelties.map((novelty) => ({ id: novelty.id, value: novelty.id, label: novelty.name }))}
+              placeholder="Seleccionar novedad"
+              options={novelties.map((novelty) => ({
+                id: novelty.id,
+                value: novelty.id,
+                label: novelty.name
+              }))}
             />
           </div>
-
+          <div>
+            <div className={styles.formTitleContainer}>
+              <ChartBar color="#666666" size={20} />
+              <Text className={styles.formTitle}>Tipo de sobrecosto</Text>
+            </div>
+            <Select
+              className={styles.select}
+              value={form.overcostId}
+              onChange={(val) => setForm({ ...form, overcostId: val })}
+              showSearch
+              optionFilterProp="label"
+              placeholder="seleccionar sobrecosto"
+              popupMatchSelectWidth={false}
+              loading={isLoadingOvercosts}
+              options={overcosts?.map((oc) => ({
+                id: oc.id,
+                value: oc.id,
+                label: oc.description
+              }))}
+            />
+          </div>
         </div>
         <div className={styles.rightSection}>
           <div>
             <div className={styles.formTitleContainer}>
-              <ChartLineUp color='#666666' size={20} />
+              <ChartLineUp color="#666666" size={20} />
               <Text className={styles.formTitle}>Cantidad</Text>
             </div>
-            <Select
+            <NumericFormat
               className={styles.select}
               value={form.quantity}
-              onChange={(val) => setForm({ ...form, quantity: val })}
-              placeholder='Escribe la cantidad'
-              options={noveltyQuantity}
+              customInput={Input}
+              thousandSeparator={"."}
+              decimalSeparator=","
+              decimalScale={0}
+              onValueChange={(values) => setForm({ ...form, quantity: values.floatValue || 0 })}
+              placeholder="Escribe la cantidad"
             />
           </div>
           <div>
             <div className={styles.formTitleContainer}>
-              <Money color='#666666' size={20} />
+              <Money color="#666666" size={20} />
               <Text className={styles.formTitle}>Sobrecosto</Text>
             </div>
-            <Input
+            <NumericFormat
+              className={styles.select}
               value={form.value}
-              onChange={(e) => setForm({ ...form, value: Number(e.target.value) })}
-              className={`${styles.input} ${styles.background}`}
-              placeholder='$0'
+              customInput={Input}
+              thousandSeparator={"."}
+              decimalSeparator=","
+              decimalScale={0}
+              prefix="$"
+              suffix=""
+              disabled
             />
           </div>
         </div>
       </div>
-      <div>
+      <div style={{ marginTop: "24px" }}>
         <div className={styles.formTitleContainer}>
-          <User color='#666666' size={20} />
+          <User color="#666666" size={20} />
           <Text className={styles.formTitle}>Observaciones</Text>
         </div>
         <Input
           value={form.observation}
           className={styles.input}
           onChange={(e) => setForm({ ...form, observation: e.target.value })}
-          placeholder='Escribir nombre'
+          placeholder="Escribir observaciones"
         />
       </div>
       <div className={styles.divider} />
       <div className={styles.evidenceContainer}>
         <div className={styles.evidenceTitleContainer}>
-          <Files size={20} color='#666666' />
+          <Files size={20} color="#666666" />
           <Text className={styles.evidenceTitle}>Evidencia</Text>
         </div>
         <div className={styles.evidences}>
           {formEvidences.map((file, index) => (
             <div key={`evidence-${index}`} className={styles.evidence}>
               <Text className={styles.evidenceTitle}>{file.name}</Text>
-              <FileArrowDown color='#141414' size={20} />
+              <FileArrowDown color="#141414" size={20} />
             </div>
           ))}
           <Upload
-            accept='.pdf, .png, .doc, .docx'
+            accept=".pdf, .png, .doc, .docx"
             showUploadList={false}
             onChange={handleUploadFile}
           >
             <div className={styles.createEvidenceContainer}>
-              <PlusCircle size={24} color='#141414' />
+              <PlusCircle size={24} color="#141414" />
               <Text className={styles.createEvidence}>Agregar documento</Text>
             </div>
           </Upload>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/services/logistics/novelty.ts
+++ src/services/logistics/novelty.ts
@@ -1,8 +1,7 @@
 import config from "@/config";
 import { GenericResponse } from "@/types/global/IGlobal";
-import { INovelty, INoveltyBody, INoveltyType } from "@/types/novelty/INovelty";
+import { INovelty, INoveltyBody, INoveltyType, IOvercost } from "@/types/novelty/INovelty";
 import { API, getIdToken } from "@/utils/api/api";
-import { UploadFile } from "antd/lib";
 import axios from "axios";
 
 export const getNoveltyDetail = async (id: number): Promise<INovelty | {}> => {
@@ -14,74 +13,89 @@ export const getNoveltyDetail = async (id: number): Promise<INovelty | {}> => {
     console.error("Error get request/details/:id/: ", error);
     throw error as any;
   }
-}
+};
 
 export const aprobeOrRejectDetail = async (id: number, isAprobe: boolean): Promise<boolean> => {
   try {
-    const { success }: GenericResponse<boolean> = await API.post('/novelty/aprobe-or-reject', {
+    const { success }: GenericResponse<boolean> = await API.post("/novelty/aprobe-or-reject", {
       id,
       isAprobe
-    })
+    });
     if (success) return true;
     return false;
   } catch (error) {
     console.error("Error get novelty/aprobe-or-reject/: ", error);
     throw error as any;
   }
-}
+};
 
 export const getNoveltyTypes = async (): Promise<INoveltyType[]> => {
   try {
-    const { success, data }: GenericResponse<INoveltyType[]> = await API.get('/novelty-type/all')
+    const { success, data }: GenericResponse<INoveltyType[]> = await API.get("/novelty-type/all");
     if (success) return data;
     return [];
   } catch (error) {
     console.error("Error get novelty-type/all/: ", error);
     throw error as any;
   }
-}
+};
 
 export const createNovelty = async (body: INoveltyBody): Promise<{ id: number } | null> => {
   try {
-    const { success, data }: GenericResponse<{ id: number }> = await API.post('/novelty/create', body);
+    const { success, data }: GenericResponse<{ id: number }> = await API.post(
+      "/novelty/create",
+      body
+    );
     if (success) return data;
     return null;
   } catch (error) {
     console.error("Error get novelty/aprobe-or-reject/: ", error);
     throw error as any;
   }
-}
+};
 
 export const updateNovelty = async (body: INoveltyBody) => {
   try {
-    const { success }: GenericResponse<boolean> = await API.post('/novelty/update', body);
+    const { success }: GenericResponse<boolean> = await API.post("/novelty/update", body);
     if (success) return true;
     return false;
   } catch (error) {
     console.error("Error update novelty/: ", error);
     throw error as any;
   }
-}
+};
 
 export const createNoveltyEvidences = async (noveltyId: number, files: File[]) => {
   const token = await getIdToken();
   try {
     const formData = new FormData();
-    formData.append('noveltyId', String(noveltyId));
+    formData.append("noveltyId", String(noveltyId));
 
     files.forEach((file) => {
       formData.append("files", file);
     });
-    const { success }: GenericResponse<boolean> = await axios.post(`${config.API_HOST}/novelty-evidence/upload`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${token}`
+    const { success }: GenericResponse<boolean> = await axios.post(
+      `${config.API_HOST}/novelty-evidence/upload`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`
+        }
       }
-    })
+    );
     if (success) return true;
     return false;
   } catch (error) {
     console.error("Error upload novelty-evidence/upload: ", error);
     throw error as any;
   }
-}
+};
+
+export const getOvercosts = async (idVehicleType: number, idCarrier: number): Promise<IOvercost[]> => {
+  const res: GenericResponse<IOvercost[]> = await API.get(
+    `/novelty-type/overcost/${idVehicleType}/${idCarrier}`
+  );
+  if (res.success) return res.data;
+  throw new Error(res.message || "ocurrió un error al obtener los costos de sobrecargo");
+};
